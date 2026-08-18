fb4aa2cbed63258f198fdf4ca9d1bf5954056c55
Picture-cat <zakirov.d.e@outlook.com>
2026-08-18 14:45:23 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -50,7 +50,7 @@ local FarmSettings = {
          ["Witches Brew"] = workspace.WorldBuyableItems.CivilianArea["Witches Brew"].Handle.PromptAttachment.ProximityPrompt,
          ["Fake Designer Sneakers"] = workspace.WorldBuyableItems.CivilianArea["Fake Designer Sneakers"].Handle.PromptAttachment.ProximityPrompt,
          ["Fake Diamond Ring"] = workspace.WorldBuyableItems.CivilianArea["Fake Diamond Ring"].Handle.PromptAttachment.ProximityPrompt,
-         ["Mona Liza Painting"] = workspace.WorldBuyableItems.CivilianArea["Mona Liza Painting"].Handle.PromptAttachment.ProximityPrompt),
+         ["Mona Liza Painting"] = workspace.WorldBuyableItems.CivilianArea["Mona Liza Painting"].Handle.PromptAttachment.ProximityPrompt,
          ["El Diablo Box"] = workspace.WorldBuyableItems.ElCapo["El Diablo Box"].Mesh.PromptAttachment.ProximityPrompt
       }
    },
